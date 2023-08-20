1b88e39cff891e53d2f8aabff71de6928b533b6f
samstickkz <samstickkz@gmail.com>
2023-08-20 20:30:10 +0100

    skills

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -96,80 +96,80 @@ const experience = () => {
         </div>
 
         <div className="expe_web">
-          <h3>Web Developer</h3>
+          <h3>Marketer</h3>
           <div className="expe_content">
             <article className="expe_details">
               <MdLibraryAddCheck className="expe-icons" />
               <div>
-                <h4>Html</h4>
+                <h4>Data Analysis</h4>
                 <small className="text-light">pro</small>
               </div>
             </article>{" "}
             <article className="expe_details">
               <MdLibraryAddCheck className="expe-icons" />
               <div>
-                <h4>Css</h4>
+                <h4>EDMs</h4>
                 <small className="text-light">pro</small>
               </div>
             </article>{" "}
             <article className="expe_details">
               <MdLibraryAddCheck className="expe-icons" />
               <div>
-                <h4>Javascript</h4>
+                <h4>Communication</h4>
                 <small className="text-light">pro</small>
               </div>
             </article>{" "}
             <article className="expe_details">
               <MdLibraryAddCheck className="expe-icons" />
               <div>
-                <h4>React</h4>
+                <h4>Social Media Marketing</h4>
                 <small className="text-light">pro</small>
               </div>
             </article>{" "}
             <article className="expe_details">
               <MdLibraryAddCheck className="expe-icons" />
               <div>
-                <h4>Rest Api</h4>
+                <h4>Digital Advertising</h4>
                 <small className="text-light">pro</small>
               </div>
             </article>{" "}
           </div>
         </div>
         <div className="expe_web">
-          <h3>Web Developer</h3>
+          <h3>UI/UX Designer</h3>
           <div className="expe_content">
             <article className="expe_details">
               <MdLibraryAddCheck className="expe-icons" />
               <div>
-                <h4>Html</h4>
+                <h4>User Interface Designer</h4>
+                <small className="text-light">Pro</small>
+              </div>
+            </article>{" "}
+            <article className="expe_details">
+              <MdLibraryAddCheck className="expe-icons" />
+              <div>
+                <h4>User Experience</h4>
+                <small className="text-light">Mid</small>
+              </div>
+            </article>{" "}
+            <article className="expe_details">
+              <MdLibraryAddCheck className="expe-icons" />
+              <div>
+                <h4>Research</h4>
                 <small className="text-light">pro</small>
               </div>
             </article>{" "}
             <article className="expe_details">
               <MdLibraryAddCheck className="expe-icons" />
               <div>
-                <h4>Css</h4>
+                <h4>Wireframes</h4>
                 <small className="text-light">pro</small>
               </div>
             </article>{" "}
             <article className="expe_details">
               <MdLibraryAddCheck className="expe-icons" />
               <div>
-                <h4>Javascript</h4>
-                <small className="text-light">pro</small>
-              </div>
-            </article>{" "}
-            <article className="expe_details">
-              <MdLibraryAddCheck className="expe-icons" />
-              <div>
-                <h4>React</h4>
-                <small className="text-light">pro</small>
-              </div>
-            </article>{" "}
-            <article className="expe_details">
-              <MdLibraryAddCheck className="expe-icons" />
-              <div>
-                <h4>Rest Api</h4>
+                <h4>Prototyping</h4>
                 <small className="text-light">pro</small>
               </div>
             </article>{" "}
