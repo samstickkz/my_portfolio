5b190ae2d872d860df90aacfaed377bbebae78e4
samstickkz <samstickkz@gmail.com>
2023-08-21 21:52:13 +0100

    ga

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Footer from './components/footer/footer'
 
 
 
+
 const app = () => {
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import ReactDom from 'react-dom'
 import App from './App'
 import './index.css'
+
 import ReactGA from 'react-ga';
 ReactGA.initialize('G-DNNJ8CLK35');
-
 ReactGA.pageview(window.location.pathname + window.location.search);
-
-
-
 ReactDom.render(<App/>, document.querySelector("#root"))
