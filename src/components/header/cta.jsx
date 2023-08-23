b98474dcdf46e2f70f2cd54674529da22a06ec28
samstickkz <samstickkz@gmail.com>
2023-08-23 09:24:40 +0100

    cv removed

--- src/components/header/cta.jsx
+++ src/components/header/cta.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/Samuel_joseph_React_flutter.pdf'
 const cta = () => {
   return (
     <div className='CTA'>
-        <a href={CV} download className='btn'> Downland CV</a>
+        <a href="#" className='btn'> Downland CV</a>
         <a href="#contact" className='btn btn_primary'>Lets Talk</a>
         
     </div>
