991f3e73ea1ac0a0d7b020ae40d1a59dde43568a
samstickkz <samstickkz@gmail.com>
2023-08-25 07:59:33 +0100

    alert

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,20 @@ import Portfolio from './components/portfolio/portfolio'
 import Testimonials from './components/testimonials/testimonials'
 import Contact from './components/contact/contact'
 import Footer from './components/footer/footer'
+import Swal from 'sweetalert2'
 
 
 
 import ReactGA from 'react-ga';
 ReactGA.initialize('G-DNNJ8CLK35');
 ReactGA.pageview(window.location.pathname + window.location.search);
+
+Swal.fire({
+  title: 'Welcome to my Portfolio!',
+  text: 'Hey its Sam,  i built this with react, look around and tell me what you think',
+  icon: 'success',
+  confirmButtonText: 'Cool'
+})
 const app = () => {
   return (
     <>
